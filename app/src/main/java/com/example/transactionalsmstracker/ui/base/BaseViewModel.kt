28b6f5d4c4792099608d357abb1fa88c514be0e9
vandanasri <sri.vandana87@gmail.com>
2020-06-09 15:03:56 +0530

    Implemented ViewModel and Live Data

--- app/src/main/java/com/example/transactionalsmstracker/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/example/transactionalsmstracker/ui/base/BaseViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.transactionalsmstracker.ui.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.transactionalsmstracker.utils.Resource
 
 class BaseViewModel : ViewModel(){
 
+
+    val messageStringId = MutableLiveData<Resource<Int>>()
+    val messageString = MutableLiveData<Resource<String>>()
 }
---
+++ app/src/main/java/com/example/transactionalsmstracker/utils/Status.kt
@@ -0,0 +1,8 @@
+package com.example.transactionalsmstracker.utils
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING,
+    UNKNOWN
+}
